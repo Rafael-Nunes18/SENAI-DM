a28362bc1f95ddc81e6a39a3643cab6e29e8d947
Rafael-Nunes18 <rafael.nunes1724@gmail.com>
2025-09-04 09:15:51 -0300

    exercises JS

---
+++ Front-End/Hello World!.js
@@ -0,0 +1,96 @@
+1/*let age = prompt("Type the age: "));
+if(age>=18){
+alert("of age");
+}
+else{
+alert("Under of age");
+}*/
+
+2/*let number = prompt("type the number: ");
+if(number>0){
+alert("Positive");
+}
+else{
+alert("Negative");
+}*/
+
+
+3/*let grade = prompt("type the grade: ");
+if(grade>=60){
+alert("Approved");
+}
+else{
+alert("Disapproved");
+}*/
+
+
+4/*let number = prompt("type the number: ");
+if(number>0){
+console.log("Positive");
+}
+else if(number==0){
+console.log("Zero");
+}
+else{
+console.log("Negative");
+}
+*/
+
+5/*let age = prompt("Type the age: ");
+if(age>0 && age<12){
+alert("Child");
+}
+else if(age>12 && age<18){
+alert("Teenager");
+}
+else if(age>=18){
+alert("Adult");
+}
+else{
+alert("age invalid");
+}*/
+
+6/*let number = prompt("Type the number: ");
+if(number%2==0){
+alert("Even");
+}
+else{
+alert("Odd");
+}*/
+
+7/*let number1= parseInt(prompt("Type the number1: "));
+let number2= parseInt(prompt("Type the number2: "));
+let operation = prompt("Type the operation: ");
+if(operation == "+"){
+alert(number1+number2);
+}
+else if(operation == "-"){
+alert(number1-number2);
+}
+else if(operation == "*"){
+alert(number1*number2);
+}
+else{
+alert(number1/number2);
+}*/
+
+8/*let number1= (prompt("Type the number1: "));
+let number2= (prompt("Type the number2: "));
+let number3= (prompt("Type the number3: "));
+
+if(number1>number2 && number1>number3){
+alert("Number 1 is the biggest");
+}
+
+else if(number2>number1 && number2>number1){
+alert("Number 2 is the biggest");
+}
+
+else if(number3>number1 && number3>number1){
+alert("Number 3 is the biggest");
+}
+
+else{
+alert("Equals");
+}*/
+
