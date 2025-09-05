70d652199279c2f98fca2e499535686382ede057
Rafael-Nunes18 <rafael.nunes1724@gmail.com>
2025-09-05 09:58:29 -0300

    JS-2

---
+++ Front-End/For -While.js
@@ -0,0 +1,22 @@
+let n = parseInt(prompt("Digite um numero: "));
+
+for(let i=1; i<=n; i++){
+console.log(i);
+}
+
+let soma = 0;
+let i = 1;
+
+while(i<=10){
+
+    console.log(i);
+    soma+=i;
+    i++;
+}
+
+console.log("soma:",soma);
+
+let numeros = [1,2,3,4,5];
+for(let i=0;i<5;i++){
+console.log(numeros[i]);
+}
